b8a85a36ccba1a783f860f774fe35895a9e58ac1
Paulo Arruda <parrudaj@gmail.com>
2025-10-27 09:18:40 -0300

    Bump RubyLLM MCP gem and remove monkey patch

--- docs/v2/CHANGELOG.swarm_sdk.md
+++ docs/v2/CHANGELOG.swarm_sdk.md
@@ -5,7 +5,8 @@ All notable changes to SwarmSDK will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [2.0.8] - 2025-10-27
+- Bump RubyLLM MCP gem and remove monkey patch
 
 ## [2.0.7] - 2025-10-26
 
--- lib/swarm_sdk.rb
+++ lib/swarm_sdk.rb
@@ -147,15 +147,6 @@ require "ruby_llm/mcp/parameter"
 
 module RubyLLM
   module MCP
-    class Parameter < RubyLLM::Parameter
-      def initialize(name, type: "string", desc: nil, required: true, default: nil, union_type: nil)
-        super(name, type: type, desc: desc, required: required)
-        @properties = {}
-        @union_type = union_type
-        @default = default
-      end
-    end
-
     module Notifications
       class Initialize
         def call
